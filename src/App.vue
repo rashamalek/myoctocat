23e29f13f98d513ac303e0d8924209a0d7427371
Alain Hélaïli <alain@carbonhub.io>
2021-05-26 21:29:33 +0200

    it displays an octocat

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <octocat/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Octocat from './components/Octocat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Octocat
   }
 }
 </script>
